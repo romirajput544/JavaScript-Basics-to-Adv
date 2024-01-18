b6d385c8a1cbc393823894a369c7fe5d689437d9
Romi kumar singh <romirajput544@gmail.com>
2024-01-18 11:28:39 +0530

    data type first commit

---
+++ 02_Datatypes and ECMA standards.js
@@ -0,0 +1,27 @@
+"use strict"; //treat all JS code as newer version
+
+// alert(3 + 3) //we are using node.js  , not browser
+
+console.log(3
+    +3); //code readability should be high , bad practise
+
+console.log("Romi");
+
+let name = "ROMI"
+let age = 18
+let isloggedIn = false
+let state = null
+
+//number => 2 to the power 53
+//bigint 
+//string => ""
+//boolean => true or false
+//null => a special data type used when we don't have any value(standalone value)
+//undefined => variable declared but not assigned any value yet
+//symbol => to find uniqueness
+
+//object
+
+console.log(typeof (null));//it is an object
+
+console.log(typeof (undefined));//it's type is undefined 
