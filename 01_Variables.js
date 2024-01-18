06d68cf6c956cd1c40bcc18a841688d520f1304c
Romi kumar singh <romirajput544@gmail.com>
2024-01-18 11:06:42 +0530

    first code in Js

---
+++ 01_Variables.js
@@ -0,0 +1,17 @@
+const accountId = 144553
+let accountEmial = "romirajput544@gmail.com"
+var accountPassword = "12345"// var doesn't follows scope , prefer not to use var
+accountCity = "Jaipur" 
+let accountState;
+
+// accountId = 2
+// console.log(accountId);
+
+
+accountEmial = "hc@hc.com"
+accountPassword = "21212121"
+accountCity = "Bengaluru"
+
+console.log(accountId);
+
+console.table([accountId , accountEmial , accountPassword , accountCity , accountState])
