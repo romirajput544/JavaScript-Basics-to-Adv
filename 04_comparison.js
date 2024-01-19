93ce19853b1681d9db08d14954d45f5ca14df020
Romi kumar singh <romirajput544@gmail.com>
2024-01-19 11:01:56 +0530

    comparisons with different data types

---
+++ 04_comparison.js
@@ -0,0 +1,22 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 2);
+// console.log(2 != 1);
+
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+// JS conerts string to no. and then compares,but you should compare similar value
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0); // Avoid these types of code
+
+// === , Strict check (checks data type)
+
+console.log("2" === 2);
