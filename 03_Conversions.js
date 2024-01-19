8d4e2ccb19ff89ec5537343ebdae9b4c2ff4c5c8
Romi kumar singh <romirajput544@gmail.com>
2024-01-19 10:28:36 +0530

    conversions first commit

---
+++ 03_Conversions.js
@@ -0,0 +1,29 @@
+let score = "33abc"
+
+//console.log(typeof(score))
+
+
+let valueInNumber = Number(score)
+//console.log(typeof(valueInNumber))
+//console.log(valueInNumber)
+
+//"33" => 33
+//"33abc" => NaN
+//true => 1
+
+
+let isLoggedIn = 1
+
+let booleanisLoggedIn = Boolean(isLoggedIn)
+console.log(booleanisLoggedIn);
+
+// 1 => true
+// 0 => false
+// "" => false
+// "Romi" => true 
+
+let someNumber = 33
+
+let stirngNumber = String(someNumber)
+console.log(stirngNumber);
+console.log(typeof stirngNumber);
