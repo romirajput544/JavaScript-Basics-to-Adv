4e266b780892396f9225460baaafd8ea2657e989
Romi kumar singh <romirajput544@gmail.com>
2024-01-20 10:41:35 +0530

    Stack and heap memory

--- datatypes_summary.js
+++ datatypes_summary.js
@@ -15,7 +15,7 @@ let userEmail;
 const id = Symbol('123')
 const anotherId = Symbol('123')
 
-console.log(id === anotherId);
+//console.log(id === anotherId);
 
  //Reference - Non primitive(Directly reference can be shared from the memory)
 
@@ -33,4 +33,31 @@ const myFunction = function(){
 }
 
 
-console.log(typeof outsideTemp);
+//console.log(typeof outsideTemp);
+
+
+//*******************Stack and Heap******************* */
+
+//Stack (Primitive) , Heap(Non-Primitive)
+
+let myYoutubename ="StocksINdepth"
+
+let anotherName = myYoutubename
+anotherName = "RomiSingh"
+
+console.log(myYoutubename);
+console.log(anotherName);
+
+
+//In Heap reference of same value is passed
+let Userone = {
+    email:"abc@gmail.com",
+    upi:"9113366721@ybl"
+}
+
+let Usertwo = Userone
+
+Usertwo.email = "xyz@gmail.com"
+
+console.log(Userone.email)
+console.log(Usertwo.email)
