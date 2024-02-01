6e05494ea6268181f3065da3edcf5fa72df3283b
Romi kumar singh <romirajput544@gmail.com>
2024-02-01 10:47:24 +0530

    fit commit

---
+++ While&Do-while.js
@@ -0,0 +1,19 @@
+//let i = 0;
+// while (i <= 10) {
+//     console.log(`Value of i is ${i}`);
+//     i++;
+// }
+
+// let myArr = ['flash','Batman','Superman']
+// let arr = 0
+// while(arr < myArr.length){
+//     console.log(`Element at index ${arr} is ${myArr[arr]}`);
+//     arr++ ;
+// }
+
+let score = 10
+
+do {
+    console.log(`score is ${score}`);
+    score++;
+} while (score <= 10);
