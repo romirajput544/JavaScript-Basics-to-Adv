2bc8a3518016748234e1b0a10ad3e49ad027264c
Romi kumar singh <romirajput544@gmail.com>
2024-02-02 11:09:02 +0530

    filters

--- 16_High order array loop.js
+++ 16_High order array loop.js
@@ -110,5 +110,56 @@ const myCoding = [
 ]
 
 myCoding.forEach( (item) => {
-    console.log(item.languageName);
+    // console.log(item.languageName);
 } )
+
+
+// const codings = ["js", "ruby", "java", "python", "cpp"]
+
+// const val = coding.forEach( (item) => {
+//     console.log(item);
+//     return item; // koi val return nai krta hai
+// } )
+
+// console.log(val);
+
+const myNums = [1,2,3,4,5,6,7,8,9,10]
+
+// const newNums = myNums.filter( (num) => {
+//     return num > 4;
+// }) 
+// 
+
+// const newNums = []
+
+// myNums.forEach((num) => {
+//     if(num > 4){
+//         newNums.push(num);
+//     } 
+// })
+// console.log(newNums);
+
+
+
+const books = [
+    { title: 'Book One', genre: 'Fiction', publish: 1981, edition: 2004 },
+    { title: 'Book Two', genre: 'Non-Fiction', publish: 1992, edition: 2008 },
+    { title: 'Book Three', genre: 'History', publish: 1999, edition: 2007 },
+    { title: 'Book Four', genre: 'Non-Fiction', publish: 1989, edition: 2010 },
+    { title: 'Book Five', genre: 'Science', publish: 2009, edition: 2014 },
+    { title: 'Book Six', genre: 'Fiction', publish: 1987, edition: 2010 },
+    { title: 'Book Seven', genre: 'History', publish: 1986, edition: 1996 },
+    { title: 'Book Eight', genre: 'Science', publish: 2011, edition: 2016 },
+    { title: 'Book Nine', genre: 'Non-Fiction', publish: 1981, edition: 1989 },
+  ];
+
+
+  let userbooks = books.filter((bk) => bk.genre === 'History')
+
+  userbooks = books.filter ( (bk) =>{return bk.publish >= 2000})
+
+  userbooks = books.filter( (bk) => { 
+    return bk.publish >= 1995 && bk.genre === "History"
+  })
+
+  console.log(userbooks)
